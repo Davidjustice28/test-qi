5390eee2bccd370ea15b05b4b14e2fdb1762d503
David Justice <105322813+Davidjustice28@users.noreply.github.com>
2023-11-20 13:13:30 -0700

    Create qualiti-script.sh

---
+++ .github/workflows/qualiti-script.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+set -ex
+
+#####################################################
+# Trigger a test run from a plan using bash         #
+# Usage: ./qualiti-script.sh <api-key> <client-id>  #
+#####################################################
+
+API_KEY=$1
+CLIENT_ID=$2
+BASE_API_URL='https://api.qualiti-dev.com'
+
+if hash apt-get 2>/dev/null; then
+  if [ "$(id -u)" -ne 0 ] && hash sudo 2>/dev/null; then
+    sudo apt-get update -y
+    sudo apt-get install -y jq curl
+  else
+    apt-get update -y && apt-get install -y jq curl
+  fi
+fi
+
+AUTH_TOKEN="$( \
+  curl -s -X POST -G "$BASE_API_URL/public/api-keys/token" \
+  -H "x-api-key: $API_KEY" \
+  -H "client-id: $CLIENT_ID" \
+  | jq -r '.token')"
+
+# Trigger test run
+TEST_RUN_ID="$( \
+curl -s -X POST -G "$BASE_API_URL/integrations/github/19/trigger-test-run" \
+  -d 'token='$AUTH_TOKEN''\
+  -d 'triggeredBy=automatic'\
+  -d 'triggerType=Deploy'\
+| jq -r '.test_run_ids[0]')"
+
+if [ "$TEST_RUN_ID" = null ] || [ -z "$TEST_RUN_ID" ]; then
+  echo 'No test trigger found.'
+  exit 0
+fi
+
+# Wait until the test run has finished
+TOTAL_ITERATION=50
+I=1
+STATUS='Pending'
+
+while [ "$STATUS" = 'Pending' ]; do
+  if [ "$I" -ge "$TOTAL_ITERATION" ]; then
+    echo 'Exit Qualiti execution; taking too long.'
+    exit 1
+  fi
+  echo "We are on iteration $I"
+
+  STATUS="$( \
+    curl -s "$BASE_API_URL/integrations/github/19/test-run-status?token=$AUTH_TOKEN&testRunId=$TEST_RUN_ID" \
+      | jq -r '.status' \
+  )"
+
+  I=$((I+1))
+
+  sleep 15
+done
+
+echo "Qualiti E2E Tests returned $STATUS"
+if [ "$STATUS" = "Passed" ]; then
+  exit 0
+fi
+
+exit 1
